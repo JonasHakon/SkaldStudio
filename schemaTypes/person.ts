85a0bba6da56439e04a4ec9f7fd66ed9b7a4df95
Jónas <jonashakon01@gmail.com>
2025-10-12 17:37:58 +0000

    updating schema

--- schemaTypes/person.ts
+++ schemaTypes/person.ts
@@ -4,8 +4,10 @@ export default defineType({
   name: 'person',
   title: 'Person',
   type: 'document',
-  fields: [
+  fields:  [
     defineField({ name: 'name', title: 'Name', type: 'string', validation: r => r.required() }),
+    defineField({ name: 'firstName', title: 'FirstName', type: 'string', validation: r => r.required() }),
+    defineField({ name: 'lastName', title: 'LastName', type: 'string', validation: r => r.required() }),
     defineField({ name: 'slug', title: 'Slug', type: 'slug', options: { source: 'name', maxLength: 96 } }),
     defineField({ name: 'picture', title: 'Picture', type: 'image', options: { hotspot: true } }),
     defineField({ name: 'secondPicture', title: 'Second Picture', type: 'image', options: { hotspot: true } }),
